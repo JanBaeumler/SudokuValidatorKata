11e121659c59fc2dc33ebbaf816202a7a088e35c
Baeumler, Jan (SE-A34) <jan.baeumler1@volkswagen.de>
2024-04-25 11:02:57 +0200

    added solution all columns are 1-9

--- src/main/kotlin/SudokuValidator.kt
+++ src/main/kotlin/SudokuValidator.kt
@@ -1,6 +1,7 @@
 fun validate(board: Array<Array<Int>>): Boolean {
     return when {
         board.all { it.distinct().size != 9 } -> false
+        board.all { it.toSet().size == 1 } -> false
         else -> true
     }
 }
